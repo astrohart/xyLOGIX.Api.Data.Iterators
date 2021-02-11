2f8453dc463cb7611ab34e9d20a22c76c77df1eb
Brian Hart <brian.hart@xylogix.net>
2021-02-11 11:10:28 -0500

    Move IteratorBase class to xyLOGIX.Data.Iterators lib

--- xyLOGIX.Data.Iterators/IteratorBase.cs
+++ xyLOGIX.Data.Iterators/IteratorBase.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using xyLOGIX.Core.Debug;
 
-namespace xyLOGIX.Data.Iterators.Interfaces
+namespace xyLOGIX.Data.Iterators
 {
    /// <summary>
    /// Implements the
