e5869f6460f61715aa6c437c39232e2d0cd281fd
Brian Hart <brian.hart@xylogix.net>
2021-02-11 11:25:35 -0500

    Create IteratorException class

---
+++ xyLOGIX.Data.Iterators.Exceptions/IteratorException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace xyLOGIX.Data.Iterators.Exceptions
+{
+   /// <summary>
+   /// Represents an exception thrown by an iterator object.
+   /// </summary>
+   public class IteratorException : Exception
+   {
+      /// <summary>
+      /// Constructs a new instance of
+      /// <see
+      ///    cref="T:xyLOGIX.Data.Iterators.Exceptions.IteratorException" />
+      /// and
+      /// returns a reference to it.
+      /// </summary>
+      /// <param name="message">
+      /// The error message that explains the reason for the exception.
+      /// </param>
+      /// <param name="innerException">
+      /// The exception that is the cause of the current exception, or a null
+      /// reference ( <see langword="Nothing" /> in Visual Basic) if no inner
+      /// exception is specified.
+      /// </param>
+      public IteratorException(string message, Exception innerException) : base(
+         message, innerException
+      ) { }
+
+      /// <summary>
+      /// Constructs a new instance of
+      /// <see
+      ///    cref="T:xyLOGIX.Data.Iterators.Exceptions.IteratorException" />
+      /// and
+      /// returns a reference to it.
+      /// </summary>
+      /// <param name="message">
+      /// The error message that explains the reason for the exception.
+      /// </param>
+      public IteratorException(string message) : base(message) { }
+   }
+}
