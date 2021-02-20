24a4a349ef0a720c4119c8a81643baba6da82929
Brian Hart <brian.hart@xylogix.net>
2021-02-19 20:22:23 -0500

    Add IteratorEventArgs and Handler

---
+++ xyLOGIX.Api.Data.Iterators.Events/IteratorErrorEventArgs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using xyLOGIX.Api.Data.Iterators.Exceptions;
+
+namespace xyLOGIX.Api.Data.Iterators.Events
+{
+    /// <summary>
+    /// Provides information for IteratorError event handlers.
+    /// </summary>
+    public class IteratorErrorEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Constructs a new instance of <see
+        /// cref="T:xyLOGIX.Api.Data.Iterators.Events.IteratorErrorEventArgs"/>
+        /// and returns a reference to it.
+        /// </summary>
+        /// <param name="exception">
+        /// (Required.) A <see
+        /// cref="T:xyLOGIX.Api.Data.Iterators.Exceptions.IteratorException"/>
+        /// that provides more information about the error.
+        /// </param>
+        public IteratorErrorEventArgs(IteratorException exception)
+        {
+            Exception = exception;
+        }
+
+        /// <summary>
+        /// Gets a reference to the <see
+        /// cref="T:xyLOGIX.Api.Data.Iterators.Exceptions.IteratorException"/>
+        /// that provides more information about the error.
+        /// </summary>
+        public IteratorException Exception { get; set; }
+    }
+}
---
+++ xyLOGIX.Api.Data.Iterators.Events/IteratorErrorEventHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace xyLOGIX.Api.Data.Iterators.Events
+{
+    /// <summary>
+    /// Represents a handler for a IteratorError event.
+    /// </summary>
+    /// <param name="sender">
+    /// Reference to the instance of the object that raised the event.
+    /// </param>
+    /// <param name="e">
+    /// A <see
+    /// cref="T:xyLOGIX.Api.Data.Iterators.Events.IteratorErrorEventArgs"/> that
+    /// contains the event data.
+    /// </param>
+    /// <remarks>
+    /// This delegate merely specifies the signature of all methods that handle
+    /// the IteratorError event.
+    /// </remarks>
+    public delegate void IteratorErrorEventHandler(object sender, IteratorErrorEventArgs e);
+}
