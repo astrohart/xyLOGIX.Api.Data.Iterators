678663fbba36bb583415ce6fad2bffa9c72d6864
Brian Hart <brian.hart@xylogix.net>
2023-11-27 00:03:38 -0700

    Latest updates at 00:03 hours MDT on 11/27/2023

--- xyLOGIX.Api.Data.Iterators.Events/IteratorErrorEventArgs.cs
+++ xyLOGIX.Api.Data.Iterators.Events/IteratorErrorEventArgs.cs
@@ -1,18 +1,52 @@
-﻿using System;
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
 using xyLOGIX.Api.Data.Iterators.Exceptions;
 
 namespace xyLOGIX.Api.Data.Iterators.Events
 {
-///  <summary> Provides information for IteratorError event handlers. </summary>
+    /// <summary> Provides information for IteratorError event handlers. </summary>
     public class IteratorErrorEventArgs : EventArgs
     {
-///  <summary> Constructs a new instance of <see cref="T:xyLOGIX.Api.Data.Iterators.Events.IteratorErrorEventArgs"/> and returns a reference to it. </summary> <param name="exception"> (Required.) A <see cref="T:xyLOGIX.Api.Data.Iterators.Exceptions.IteratorException"/> that provides more information about the error. </param>
-        public IteratorErrorEventArgs(IteratorException exception)
-        {
-            Exception = exception;
-        }
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed once only
+        /// for the
+        /// <see cref="T:xyLOGIX.Api.Data.Iterators.Events.IteratorErrorEventArgs" />
+        /// class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance being
+        /// created or before any static members are referenced.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static IteratorErrorEventArgs() { }
 
-///  <summary> Gets a reference to the <see cref="T:xyLOGIX.Api.Data.Iterators.Exceptions.IteratorException"/> that provides more information about the error. </summary>
+        /// <summary>
+        /// Creates a new instance of
+        /// <see cref="T:xyLOGIX.Api.Data.Iterators.Events.IteratorErrorEventArgs" /> and
+        /// returns a reference to it.
+        /// </summary>
+        [Log(AttributeExclude = true)]
+        public IteratorErrorEventArgs() { }
+
+        /// <summary>
+        /// Constructs a new instance of
+        /// <see cref="T:xyLOGIX.Api.Data.Iterators.Events.IteratorErrorEventArgs" /> and
+        /// returns a reference to it.
+        /// </summary>
+        /// <param name="exception">
+        /// (Required.) A
+        /// <see cref="T:xyLOGIX.Api.Data.Iterators.Exceptions.IteratorException" /> that
+        /// provides more information about the error.
+        /// </param>
+        [Log(AttributeExclude = true)]
+        public IteratorErrorEventArgs(IteratorException exception)
+            => Exception = exception;
+
+        /// <summary>
+        /// Gets a reference to the
+        /// <see cref="T:xyLOGIX.Api.Data.Iterators.Exceptions.IteratorException" /> that
+        /// provides more information about the error.
+        /// </summary>
         public IteratorException Exception { get; set; }
     }
 }
